6594c88edac4fcd248f9964fb27acd701849bb98
George Githae <gabs_githae@outlook.com>
2025-09-23 13:16:20 +0300

    Added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports={content:["./pages/**/*.{js,jsx}","./components/**/*.{js,jsx}"],theme:{extend:{}},plugins:[]};
+module.exports={content:["./pages/**/*.{js,jsx}","./components/**/*.{js,jsx}"],theme:{extend:{
+    colors: {
+        brand: {
+        primary: '#0B5CAB',
+        secondary: '#1F2937',
+        green: '#16A34A',
+        orange: '#F97316',
+        bg: '#F3F4F6'
+        }
+    }    
+}},plugins:[]};
